54e151b09ba2a2fbc4f4445a717feff0dd71a042
Leam Hall <leamhall@gmail.com>
2018-01-02 19:51:58 -0500

    Added build_chapters.rb.

---
+++ bin/build_chapters.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'optparse'
+
+chapter_dir = 'chapters'
+total_dir   = 'total'
+book_dir    = 'book'
+
+options = Hash.new(0)
+option_parser = OptionParser.new do |opts|
+  opts.on('-t', '--title TITLE', 'Title of the book') do |t|
+    options[:title] = t
+  end
+end
+option_parser.parse!
+
+if options[:title] == 0
+  book_name = 'Book'
+else
+  book_name = options[:title]
+end
+puts("book name is #{book_name}.")
+
+if File.directory?(total_dir)
+  FileUtils.rm_rf(total_dir)
+end
+Dir.mkdir(total_dir)
+
+if File.directory?(book_dir)
+  FileUtils.rm_rf(book_dir)
+end
+Dir.mkdir(book_dir)
+
+if File.directory?(chapter_dir)
+  chapter_files = Dir.entries(chapter_dir)
+  chapter_files.sort!
+else
+  puts("Need chapters.")
+  exit
+end
+
+chapter_files.each do |chapter_file|
+  if chapter_file.end_with?('.txt')
+    chapter = File.read("#{chapter_dir}/#{chapter_file}")
+    chapter_count = chapter_file[0..1]
+    File.open("#{total_dir}/Chapter_#{chapter_count}", "a") do |file|
+      file.write(chapter)
+    end
+    File.open("#{book_dir}/#{book_name}", "a+") do |file|
+      file.write(chapter)
+    end
+  end
+end
