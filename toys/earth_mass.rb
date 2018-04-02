50ae2fef56f4d99d4e62d98270a8f6924fc8ff09
Leam Hall <leamhall@gmail.com>
2018-04-02 06:47:52 -0400

    Double-checking in earth_mass.rb.

--- toys/earth_mass.rb
+++ toys/earth_mass.rb
@@ -12,9 +12,21 @@ surface_km2 = 51007200
 volume_km3  = 1.08321e12
 volume_m3   = volume_km3 * K * K * K * K
             # (K*K*K) to convert to m3, then one more for m to cm.
+mass_calc   = volume_m3 * density
 
-puts("%E" % (volume_m3 * density))
 puts(mass_kg)
 
 gravity     = G * mass_kg / ( radius_m * radius_m)
-puts(gravity)
+puts("%2.3f" % gravity)
+puts("Volume (m3): %e" % volume_m3)
+puts("Going the other way.")
+# If mass = density * volume
+# then
+#   volume  = mass / density
+#   density = mass / volume
+
+puts("Calculated mass: %e" % mass_calc)
+puts("Calculated dendity %2.3f" % ( mass_calc / volume_m3))
+puts("Calculated volume (m3): %e" % (mass_calc / density))
+mass_diff = (mass_kg - mass_calc).abs
+puts("Difference between calculated and wikipedia mass: %e" % ((mass_calc - mass_kg).abs) )
