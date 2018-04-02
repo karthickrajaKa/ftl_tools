904712e1fc705f03fdd61e22587bdf54906879b0
Leam Hall <leamhall@gmail.com>
2018-04-01 20:11:19 -0400

    earth_mass.rb

--- toys/earth_mass.rb
+++ toys/earth_mass.rb
@@ -1,5 +1,20 @@
+#!/usr/bin/env ruby
 
-earth_mass  = 5.97237e24
-radius_km   = 6371
+# Just testing base math using Earth's measurements.
+# Measurements are from wikipedia "Earth"
+G           = 6.674e-11   # Universoal constant
+K           = 1000        # To convert from kilo to whatever-o
+mass_kg     = 5.97237e24  # kg
+density     = 5.514       # g/cm3
+radius_km   = 6371        # 
+radius_m    = radius_km * K
 surface_km2 = 51007200
 volume_km3  = 1.08321e12
+volume_m3   = volume_km3 * K * K * K * K
+            # (K*K*K) to convert to m3, then one more for m to cm.
+
+puts("%E" % (volume_m3 * density))
+puts(mass_kg)
+
+gravity     = G * mass_kg / ( radius_m * radius_m)
+puts(gravity)
