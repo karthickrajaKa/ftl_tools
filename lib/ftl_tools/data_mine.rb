fbfee6620fa89cb016195dd02a0360e8fb4480e6
Leam Hall <leamhall@gmail.com>
2019-07-06 07:06:18 -0400

    Made DataMine use its own array_from_file method.

--- lib/ftl_tools/data_mine.rb
+++ lib/ftl_tools/data_mine.rb
@@ -7,20 +7,8 @@ module FTLTools
 
     # Return a string, without newline, from a multi-line file.
     def get_random_line_from_file(file)
-      begin
-        lines = Array.new
-        File.open(file) { |file|
-          file.readlines.each { |line|
-            line = line.chomp 
-            next if line.start_with?('#') or line.length < 2
-            lines << line 
-          }
-        }
-        lines.sample
-      rescue IOError => e
-        puts "File error:  #{e}."
-        exit
-      end
+      lines = array_from_file(file)
+      lines.sample
     end  
     module_function :get_random_line_from_file
 
