8edb95060797d1ad651f35022931ea37645639dc
Leam Hall <leamhall@gmail.com>
2019-07-10 07:05:00 -0400

    Basic test for webapp work.

---
+++ spec/unit/crewgen_spec.rb
@@ -0,0 +1,29 @@
+# crewgen_spec.rb
+
+require 'rack/test'
+require 'ftl_tools'
+
+module FTLTools
+
+  RecordResult = Struct.new(:success?)
+
+  RSpec.describe 'crewgen' do
+    include Rack::Test::Methods
+   
+    def app
+      CrewGen.new
+    end 
+
+    it 'returns a 200 status for /crew' do
+      get '/crew'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'has a pilot' do
+      get '/crew'
+      expect(last_response.body).to include('Pilot')
+    end
+
+  end
+
+end    
